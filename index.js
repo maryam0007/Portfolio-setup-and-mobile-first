fffe915d52133a6b570ff70a1523222d8c23103e
maryam0007 <maryammicroverse@gmail.com>
2023-04-10 19:53:10 +0500

    updated mobile menu html and css

---
+++ index.js
@@ -0,0 +1,18 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.getElementById('menu-');
+  const button = document.getElementById('mob-barss');
+  const closebtn = document.getElementById('closed');
+  const menuitems = document.getElementById('mlist');
+
+  button.onclick = function () {
+    menu.style.display = 'block';
+  };
+
+  closebtn.onclick = function () {
+    menu.style.display = 'none';
+  };
+
+  menuitems.onclick = function () {
+    menu.style.display = 'none';
+  };
+}); 
