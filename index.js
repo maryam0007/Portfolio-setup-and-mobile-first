b2f03e9367cba233fc60b8f41a4a858293d4a852
maryam0007 <maryammicroverse@gmail.com>
2023-04-21 14:09:09 +0500

    Made a bug

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 submit.addEventListener('click', (event) => {
   const email = document.getElementById('email').value;
-  const error = document.querySelector('.error');
+  const error = document.querySelector('error');
   const isLowerCase = (str) => str === str.toLowerCase();
   if (!isLowerCase(email)) {
     event.preventDefault();
